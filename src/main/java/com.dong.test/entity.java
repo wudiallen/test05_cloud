b6f3f944d7c94046f6dc800bfaf53228244c4b08
小董 <66095960+wudiallen@users.noreply.github.com>
2021-06-05 22:50:31 +0800

    ok

--- src/main/java/com.dong.test/entity.java
+++ src/main/java/com.dong.test/entity.java
@@ -13,5 +13,6 @@ public class entity {
         System.out.println("hot  this is a git programer4");
         System.out.println("hot  this is a git programer5");
         System.out.println("hot  this is a git programer6");
+          System.out.println("hot  this is a git programer7");
     }
 }
