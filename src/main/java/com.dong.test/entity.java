b0e217b043b591ca84ecf097da7fe0b1393b6538
dsc <www.1169889740@qq.com>
2021-06-05 22:18:27 +0800

    s commit

--- src/main/java/com.dong.test/entity.java
+++ src/main/java/com.dong.test/entity.java
@@ -6,5 +6,6 @@ package com.dong.test;
 public class entity {
     public static void main(String[] args) {
         System.out.println("this is a git programer test");
+        System.out.println("this is a git programer test2");
     }
 }
