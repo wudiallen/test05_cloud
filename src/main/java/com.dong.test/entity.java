5d1a5aba011bd4dac9f748a5491feefb86c42d90
dsc <www.1169889740@qq.com>
2021-06-05 22:41:33 +0800

    hot commit3

--- src/main/java/com.dong.test/entity.java
+++ src/main/java/com.dong.test/entity.java
@@ -12,5 +12,6 @@ public class entity {
         System.out.println("this is a git programer4");
         System.out.println("hot  this is a git programer4");
         System.out.println("hot  this is a git programer5");
+        System.out.println("hot  this is a git programer6");
     }
 }
